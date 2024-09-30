a2c8358e228134d4d8243138b1d2ccb1f1d036ac
John Naquin <79560056+John-Naquin@users.noreply.github.com>
2024-09-30 18:08:06 -0500

    Update Hero.js

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -53,7 +53,7 @@ const Hero = () => {
 
         <button 
           className="w-14 h-14 bg-transparent rounded-full border-2 border-blue-600 hover:bg-blue-600 hover:border-blue-800 shadow-lg transform hover:scale-105 transition-all duration-300 ease-in-out"
-          onClick={() => handleButtonClick('https://1drv.ms/b/c/24bcd1f502d345be/EbHeu58qVuBOtnUaM0CtmVkB-03pGURCd9E1Y-CNrx0w2Q?e=XH9Ypr')}
+          onClick={() => handleButtonClick('https://1drv.ms/b/c/24bcd1f502d345be/EceBPcim8n9KhIkjBfck41wBtsMS1LzDjxU4OqH4tSZrng?e=4k53Pn')}
         >
           <img src={resumeIcon} alt="Resume" className="w-8 h-8 mx-auto" />
         </button>
